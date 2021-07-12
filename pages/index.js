71144b89a8e0565bb95d298e4c65cd2d344f25e7
Hebert324 <hebertrocha76@gmail.com>
2021-07-12 14:26:07 -0300

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+background-color: #ffffff;
+border-radius: 8px;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <main>
+      <Box>Imagem</Box>
+      <Box>Bem vindo</Box>
+      <Box>Comunidades</Box>
+    </main>
+  )
 }
