1c504d83d3a0ba2feb47422577696c093a69f860
Hebert324 <hebertrocha76@gmail.com>
2021-07-13 11:06:01 -0300

    add form, is add comunits

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import { useState, useEffect } from 'react'
 
@@ -8,6 +8,16 @@ function ProfileSideBar(props) {
   return (
     <Box>
       <a target="_blank" href={`https://github.com/${props.githubUser}`}><img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} /></a>
+
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>{props.githubUser}</a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
@@ -30,7 +40,8 @@ export default function Home() {
 
   const follower = followers.slice(0, 6)
 
-  const comunits = ''
+  // comunidades
+  const comunits = ["alurakut"]
 
   return(
     <>
@@ -42,34 +53,84 @@ export default function Home() {
 
       <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
         <Box>
-          <h1 className="title">Bem vindo(a)</h1>
+          <h1 className="title">Bem vindo(a), {githubUser}</h1>
           <OrkutNostalgicIconSet />
+        </Box>
+
+        <Box>
+          <h2 className="subTitle">Oque você deseja fazer?</h2>
+
+          <form onSubmit={(e) => {e.preventDefault()}}>
+            <div>
+              <input 
+              placeholder="Qual vai ser o nome da sua comunidade?" 
+              name="title" 
+              aria-label="Qual vai ser o nome da sua comunidade?"
+              type="text"
+              />
+            </div>
+
+            <div>
+            <input 
+              placeholder="Coloque a URL para usarmos de capa" 
+              name="title" 
+              aria-label="Coloque uma URL para usarmos de capa."
+            />
+            </div>
+
+            <button>
+              Criar Comunidade
+            </button>
+
+          </form>
+
         </Box>
       </div>
 
       <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-        <ProfileRelationsBoxWrapper>
 
-          <h2 className="smallTitle">Meus amigos <span style={{ color: '#2E7BB4' }}>({followers.length})</span></h2>
+      {/* Amigos */}
+      <ProfileRelationsBoxWrapper>
 
-          <ul>
-            {follower.map((follower) => {
-              return (
-                <li>
-                  <a href={`https://github.com/${follower.login}`} key={follower.id} target="_blank">
-                    <img src={`https://github.com/${follower.login}.png`} />
-                    <span>{follower.login}</span>
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
+        <h2 className="smallTitle">Meus amigos <span style={{ color: '#2E7BB4' }}>({followers.length})</span></h2>
 
-        </ProfileRelationsBoxWrapper>
+        <ul>
+          {follower.map((follower) => {
+            return (
+              <li>
+                <a href={`https://github.com/${follower.login}`} key={follower.id} target="_blank">
+                  <img src={`https://github.com/${follower.login}.png`} />
+                  <span>{follower.login}</span>
+                </a>
+              </li>            
+            )
+          })}
+        </ul>
 
-        <Box>
+        <hr />
+
+      </ProfileRelationsBoxWrapper>
+
+      {/* comunidades */}
+      <ProfileRelationsBoxWrapper>
+
         <h2 className="smallTitle">Comunidades <span style={{ color: '#2E7BB4' }}>({comunits.length})</span></h2>
-        </Box>
+        
+        <ul>
+          {comunits.map((comunit) => {
+            return (
+              <li>
+                <a href={`https://github.com/${comunit}`} key={comunit} target="_blank">
+                  <img src={`http://placehold.it/300x300`} />
+                  <span>{comunit}</span>
+                </a>
+              </li>            
+            )
+          })}
+        </ul>
+
+        <hr />
+      </ProfileRelationsBoxWrapper>
       </div>
     </MainGrid>
     </>
